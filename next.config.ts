cf71c4efd15a35cf7d11955bed485654366199d2
raweeroj thokaeo <raweeroj.tk@gmail.com>
2026-07-04 13:44:14 +0700

    init next

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  images: { unoptimized: true }, // needed if you use next/image
+  basePath: '/umi-design-sample', // needed so assets load correctly on github.io/repo-name
 };
 
 export default nextConfig;
